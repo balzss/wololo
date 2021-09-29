7deafe6cb3b00146ad068c7d161645335d5ed9f3
balazs.saros <balazs.saros@gmail.com>
2021-09-29 14:34:11 +0200

    waiting for wololo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const animationDuration = 2650;
+
 function App() {
+  const audio = new Audio("/wololo-sound.mp3")
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [showRed, setShowRed] = useState(false);
+
+  const playSound = () => {
+    if (isPlaying) return;
+    audio.play();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg-img">
+        <img src="/aoe1-bg.jpeg" alt="" />
+      </div>
+      <div className="gif-container">
+        {showRed ? (
+          <div
+            onClick={() => {
+              playSound();
+              setIsPlaying(true);
+              setTimeout(() => {
+                setIsPlaying(false);
+                setShowRed(false);
+              }, animationDuration);
+            }}
+          >
+            {isPlaying ? (
+              <img src="/wololo-red-to-blue.gif" className="App-logo" alt="logo" />
+            ) : (
+              <img src="/red-still.gif" className="App-logo" alt="logo" />
+            )}
+          </div>
+        ) : (
+          <div onClick={() => {
+            playSound();
+            setIsPlaying(true);
+            setTimeout(() => {
+              setIsPlaying(false);
+              setShowRed(true);
+            }, animationDuration - 100);
+          }}
+          >
+            {isPlaying ? (
+              <img src="/wololo-blue-to-red.gif" className="App-logo" alt="logo" />
+            ) : (
+              <img src="/blue-still.gif" className="App-logo" alt="logo" />
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
